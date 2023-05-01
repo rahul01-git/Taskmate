7051fe610e56010ed3d9361aa6c31083bb0a4493
rahul01-git <rahulneupane0090@gmail.com>
2023-05-01 22:03:42 +0545

    use of local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import ShowTask from "./components/ShowTask";
 function App() {
-  const [tasklist, setTasklist] = useState([]);
+  const [tasklist, setTasklist] = useState(
+    JSON.parse(localStorage.getItem("tasklist")) || []
+  );
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('tasklist',JSON.stringify(tasklist));
+  }, [tasklist]);
 
   return (
     <div className="container">
